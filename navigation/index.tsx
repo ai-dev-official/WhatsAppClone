72f77fd61e886fbf09e97fb02fa78cf766cd37e9
ai-codesPro <ai.codespro@gmail.com>
2021-07-24 17:41:48 +0100

    first commit

---
+++ navigation/index.tsx
@@ -0,0 +1,95 @@
+/**
+ * If you are not familiar with React Navigation, check out the "Fundamentals" guide:
+ * https://reactnavigation.org/docs/getting-started
+ *
+ */
+import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import * as React from 'react';
+import { View, ColorSchemeName } from 'react-native';
+import { Octicons, MaterialCommunityIcons, FontAwesome5, MaterialIcons, } from '@expo/vector-icons';
+/* import { Feather } from '@expo/vector-icons'; */
+        
+
+import ChatRoomScreen from '../screens/ChatRoomScreen';
+import NotFoundScreen from '../screens/NotFoundScreen';
+import { RootStackParamList } from '../types';
+import MainTabNavigator from './MainTabNavigator';
+import LinkingConfiguration from './LinkingConfiguration';
+import Colors from '../constants/Colors';
+
+export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
+  return (
+    <NavigationContainer
+      linking={LinkingConfiguration}
+      theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <RootNavigator />
+    </NavigationContainer>
+  );
+}
+
+// A root stack navigator is often used for displaying modals on top of all other content
+// Read more here: https://reactnavigation.org/docs/modal
+const Stack = createStackNavigator<RootStackParamList>();
+
+function RootNavigator() {
+  return (
+    <Stack.Navigator 
+    screenOptions={{ 
+        headerStyle: {
+         backgroundColor: Colors.light.tint, 
+         shadowOpacity: 0,
+         elevation: 0,
+        },
+
+        headerTintColor: Colors.light.background,
+        headerTitleAlign: 'left',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}>
+
+      <Stack.Screen 
+        name="Root" 
+        component={MainTabNavigator} 
+        options={{
+          title: "WhatsApp",
+          headerRight: () => (
+            <View style={{
+                flexDirection: 'row',
+                width: 60,
+                justifyContent: 'space-between',
+                marginRight: 10,
+                }}>
+
+                <Octicons name="search" size={24} color={'white'} />
+                <MaterialCommunityIcons name="dots-vertical" color={'white'} size={24} />
+            </View>
+          )
+        }}
+      />
+
+       <Stack.Screen
+        name="ChatRoom"
+        component={ChatRoomScreen}
+        options={({ route })  => ({
+          title: route.params.name,
+          headerRight: () => (
+            <View style={{
+              flexDirection: 'row',
+              width: 100,
+              justifyContent: 'space-between',
+              marginRight: 10,
+            }}>
+              <FontAwesome5 name="video" size={22} color={'white'} />
+              <MaterialIcons name="call" size={22} color={'white'} />
+              <MaterialCommunityIcons name="dots-vertical" size={22} color={'white'} />
+            </View>
+          )
+        })}
+      />
+
+      <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
+    </Stack.Navigator>
+  );
+}
